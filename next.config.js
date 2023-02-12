9f40326e3ec7a120bc0c21c6ea114597d9815267
Graham Hoyes <accounts@grahamhoyes.com>
2023-02-12 15:11:51 -0500

    Enble edge runtime

--- next.config.js
+++ next.config.js
@@ -58,6 +58,9 @@ module.exports = withBundleAnalyzer({
   eslint: {
     dirs: ["pages", "components", "lib", "layouts", "scripts"],
   },
+  experimental: {
+    runtime: "edge",
+  },
   async headers() {
     return [
       {
