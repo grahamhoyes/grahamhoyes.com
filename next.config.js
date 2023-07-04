4b3c68c4b49b89e017060a9c9ef671d70732da44
Graham Hoyes <accounts@grahamhoyes.com>
2023-07-03 20:12:28 -0400

    Remove preact
    Preact is currently incompatible with React 18 hooks, and seemingly some server functions
    stuff. Removing it increases the bundle size by ~2x, but interactivity doesn't work
    with preact left in.

--- next.config.js
+++ next.config.js
@@ -82,21 +82,11 @@ module.exports = () => {
     //     },
     //   ];
     // },
-    webpack: (config, { dev, isServer }) => {
+    webpack: (config) => {
       config.module.rules.push({
         test: /\.svg$/,
         use: ["@svgr/webpack"],
       });
-
-      if (!dev && !isServer) {
-        // Replace React with Preact only in client production build
-        Object.assign(config.resolve.alias, {
-          "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-          react: "preact/compat",
-          "react-dom/test-utils": "preact/test-utils",
-          "react-dom": "preact/compat",
-        });
-      }
 
       return config;
     },
